bba0b8a5f21846273c19f5879a492a80673da3b1
Bernardo Porto Veronese <bernardo.veronese@arcca.io>
2024-01-10 16:40:22 -0300

    Add method to sample from distribution with inversion technique

--- src/inference/utils.py
+++ src/inference/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.integrate import cumulative_trapezoid
 from astropy.cosmology import FlatLambdaCDM
 from .constants import Om0
 
@@ -13,3 +14,53 @@ def lognormal(x, mu, sigma):
 
 def flat_cosmology(H0):
     return FlatLambdaCDM(H0, Om0)
+
+
+def merger_rate(z, alpha, beta, c):
+    return np.power(1 + z, alpha) / (1.0 + np.power((1 + z) / c, beta))
+
+
+def sample_from_func(n, func, x, *args, **kwargs):
+    prob = func(x, *args, **kwargs)
+    cdf = cumulative_trapezoid(prob, x)
+    max_cdf = cdf[-1]
+    percentiles = np.random.uniform(high=max_cdf, size=n)
+    return x[np.searchsorted(cdf, percentiles)]
+
+
+class EventGenerator:
+    def __init__(self, H0, z_draw_max, dl_th) -> None:
+        self.cosmology = flat_cosmology(H0)
+        self.z_draw_max = z_draw_max
+        self.dl_th = dl_th
+        self.drawn_redshifts = None
+
+    def luminosity_distance(self, cosmology, z):
+        return cosmology.luminosity_distance(z).to("Mpc").value
+
+    def uniform_p_rate(self, z_gal, z_draw_max):
+        # Uniform merger probability on 0 < z < z_draw_max
+        p_rate = np.zeros_like(z_gal)
+        p_rate[z_gal <= z_draw_max] = 1
+        p_rate /= np.sum(p_rate)
+        return p_rate
+
+    def draw_redshifts(self, z_gal, n):
+        p_rate = self.uniform_p_rate(z_gal, self.z_draw_max)
+        self.drawn_redshifts = np.random.choice(z_gal, n, p=p_rate)
+
+    def from_catalog(self, cosmology, sigma_dl, noise=None):
+        drawn_gw_zs = self.drawn_redshifts
+        if drawn_gw_zs is None:
+            raise ValueError("drawn_redshifts method must be called before.")
+
+        # Convert them into "true" gw luminosity distances using a fiducial cosmology
+        drawn_gw_dls = self.luminosity_distance(cosmology, drawn_gw_zs)
+        _noise = noise if noise is not None else np.random.standard_normal(len(drawn_gw_dls))
+
+        # Convert true gw luminosity distances into measured values
+        # drawn from a normal distribution consistent with the GW likelihood
+        sigma = drawn_gw_dls * sigma_dl
+        observed_gw_dls = _noise * sigma + drawn_gw_dls
+        # Filter events whose dL exceeds threshold
+        return observed_gw_dls[observed_gw_dls < self.dl_th]
