84a9188e1398d5f3b9fc6277309376ecbd77a480
Bernardo Porto Veronese <bernardo.veronese@arcca.io>
2024-01-11 16:03:12 -0300

    Add method docstrings

--- src/inference/utils.py
+++ src/inference/utils.py
@@ -1,11 +1,30 @@
 import numpy as np
 from scipy.integrate import cumulative_trapezoid, simpson
 from astropy.cosmology import FlatLambdaCDM
-from .constants import Om0
+from .constants import Om0, eps
 
 
-def normalize(y, x):
-    return y / simpson(y, x)
+def normalize(y, x, safe=False):
+    """Return an array normalized with its integral.
+
+    Parameters
+    ----------
+    y : array_like
+        The array to normalize
+    x : array_like
+        The axis to perform the integration
+    safe : bool, optional
+        Whether to prevent division by zero-norm, by default False
+
+    Returns
+    -------
+    array_like
+        The normalized array
+    """
+    norm = simpson(y, x)
+    if safe:
+        norm += eps
+    return y / norm
 
 
 def gaussian(x, mu, sigma):
@@ -25,6 +44,22 @@ def merger_rate(z, alpha, beta, c):
 
 
 def sample_from_func(n, func, x, *args, **kwargs):
+    """Return samples from a function with known PDF using the inversion technique.
+
+    Parameters
+    ----------
+    n : int
+        The number of samples to return
+    func : function
+        The distribution PDF. Does not need to be normalized
+    x : array_like
+        The array from which the samples will be drawn
+
+    Returns
+    -------
+    array_like
+        The array of samples with length n
+    """
     prob = func(x, *args, **kwargs)
     cdf = cumulative_trapezoid(prob, x)
     max_cdf = cdf[-1]
