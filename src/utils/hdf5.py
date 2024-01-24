5be88526c226922cd0e490e128ed84f35bfac9d3
Bernardo Porto Veronese <bernardo.veronese@arcca.io>
2024-01-24 13:57:29 -0300

    Add utility methods

---
+++ src/utils/hdf5.py
@@ -0,0 +1,24 @@
+import h5py
+
+
+def create_or_overwrite_dataset(file: h5py.File, dataset, data):
+    """
+    Overwrite existing dataset with new data for a hdf5 file.
+    If such dataset does not exist, create one and populate it
+    """
+    try:
+        # See https://stackoverflow.com/a/22925117
+        old_data = file[dataset]
+        old_data[...] = data
+    except KeyError:
+        file.create_dataset(dataset, data=data)
+
+
+def write_to_file(file: str, **data):
+    with h5py.File(file, "a") as f:
+        attrs = data.pop("attrs", None)
+        for key, dataset in data.items():
+            create_or_overwrite_dataset(f, key, data=dataset)
+
+        if attrs is not None and isinstance(attrs, dict):
+            f.attrs.update(**attrs)
