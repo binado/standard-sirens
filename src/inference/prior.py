34766aa26829d6689b41888786bc8de596e1c9ff
Bernardo Porto Veronese <bernardo.veronese@arcca.io>
2024-01-30 21:07:27 -0300

    Add UniformPrior class

---
+++ src/inference/prior.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+class UniformPrior:
+    def __init__(self, prior_min, prior_max) -> None:
+        if len(prior_max) != len(prior_min):
+            raise ValueError("Prior bounds do not have the same dimension")
+
+        self.prior_min = prior_min
+        self.prior_max = prior_max
+        self.ndim = len(prior_max)
+        self.norm = np.prod(prior_max - prior_min)
+        if self.norm < 0:
+            raise ValueError("Prior has negative norm")
+
+        self.log_norm = -np.log(self.norm)
+
+    @property
+    def interval(self):
+        return self.prior_max - self.prior_min
+
+    def log_prior(self, prior):
+        if np.all(prior >= self.prior_min) and np.all(prior <= self.prior_max):
+            return self.log_norm
+        return -np.inf
