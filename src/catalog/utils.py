47ece5bca11162b420f325f06296ed3aa61ab63f
Bernardo Porto Veronese <bernardo.veronese@arcca.io>
2024-02-02 17:27:18 -0300

    Reorganize catalog related classes and methods

--- src/catalog/utils.py
+++ src/catalog/utils.py
@@ -5,6 +5,39 @@ import healpy as hp
 DEFAULT_DATASETS = ["ra", "dec", "z", "mass", "skymap_indices"]
 
 
+def traverse_file(group, node, target):
+    if isinstance(node, dict):
+        for key, obj in node.items():
+            traverse_file(group[key], obj, target)
+    elif isinstance(node, list):
+        for dataset in node:
+            setattr(target, dataset, group[dataset][()])
+    elif isinstance(node, str):
+        setattr(target, node, group[node][()])
+
+
+def draw_galaxies(skymap, n_dir, alpha, n_min):
+    """
+    Return all galaxies within each randomly chosen n_dir directions.
+
+    Each direction is guaranteed to have at least n_min galaxies
+    """
+    # Generate large enough sample of directions as some might be rejected
+    n_sim = 10 * n_dir
+    theta = np.random.uniform(0, np.pi / 2, n_sim)
+    phi = np.random.uniform(0, 2 * np.pi, n_sim)
+
+    current_dir = 0
+    galaxies = []
+    for i in range(n_sim):
+        _, galaxies_at_direction = skymap.indices_at_direction(theta[i], phi[i], alpha)
+        if np.sum(galaxies_at_direction) > n_min:
+            current_dir += 1
+            galaxies.append(galaxies_at_direction)
+        if current_dir >= n_dir:
+            return galaxies
+
+
 class Skymap:
     def __init__(self, nside, ra, dec, **kwargs) -> None:
         self.ra = ra
@@ -39,63 +72,9 @@ class Skymap:
 
 
 class GalaxyCatalog:
-    def __init__(self, filename) -> None:
+    def __init__(self, filename: str) -> None:
         self.filename = filename
-        self.file = h5py.File(filename, "r")
 
-    def dataset(self, group: str, name: str):
-        path = "{0}/{1}".format(group, name)
-        # The '()' slice retrieves all data from the dataset
-        return self.file[path][()]
-
-    def close(self):
-        self.file.close()
-
-    def npix(self, nside):
-        return hp.nside2npix(nside)
-
-    def get_skymap(self, nside, ra, dec, **kwargs):
-        return hp.ang2pix(nside, dec + np.pi / 2, ra, **kwargs)
-
-    def galaxy_counts(self, skymap_indices):
-        """
-        Return an array with the number of galaxies within each pixel
-        """
-        nonempty_pixels, counts = np.unique(skymap_indices, return_counts=True)
-        skymap = np.zeros(self.npix)
-        for pixel, count in zip(nonempty_pixels, counts):
-            skymap[pixel] = count
-
-        return skymap
-
-    def galaxies_at_direction(self, skymap_indices, theta, phi, alpha):
-        """
-        Get all galaxy redshifts at an angular distance alpha from a sky direction
-        (theta, phi).
-        """
-        center = hp.ang2vec(theta, phi)
-        # Get corresponding HEALPIX pixels
-        ipix_within_disc = hp.query_disc(nside=self.nside, vec=center, radius=alpha)
-        galaxies_in_ipix_array = np.isin(skymap_indices, ipix_within_disc)
-        return ipix_within_disc, galaxies_in_ipix_array
-
-    def draw_galaxies(self, skymap_indices, n_dir, alpha, n_min):
-        """
-        Return all galaxies within each randomly chosen n_dir directions.
-
-        Each direction is guaranteed to have at least n_min galaxies
-        """
-        # Generate large enough sample of directions as some might be rejected
-        n_sim = 10 * n_dir
-        theta = np.random.uniform(0, np.pi / 2, n_sim)
-        phi = np.random.uniform(0, 2 * np.pi, n_sim)
-
-        current_dir = 0
-        galaxies = []
-        for i in range(n_sim):
-            _, galaxies_at_direction = self.galaxies_at_direction(skymap_indices, theta[i], phi[i], alpha)
-            if np.sum(galaxies_at_direction) > n_min:
-                current_dir += 1
-                galaxies.append(galaxies_at_direction)
-            if current_dir >= n_dir:
-                return galaxies
+    def get(self, dataset):
+        with h5py.File(self.filename, "r") as f:
+            return f[dataset][()]
