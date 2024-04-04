f3d58247baa4004770069702735221c719e67221
Bernardo Porto Veronese <bernardo.veronese@arcca.io>
2024-04-04 13:50:30 -0300

    Finish TaylorF2 waveform approximant implementation

--- src/gw/utils.py
+++ src/gw/utils.py
@@ -1,6 +1,10 @@
 import numpy as np
-from astropy.constants import c, G
+from astropy import constants as const
+from astropy import units as u
 from scipy.integrate import simpson
+
+mpc_to_m = 1e6 * u.pc.to(u.m)
+msun = const.M_sun.value
 
 
 def symmetric_mass_ratio(m1, m2):
@@ -8,7 +12,7 @@ def symmetric_mass_ratio(m1, m2):
 
 
 def mchirp(m1, m2):
-    return (m1 * m2) ** 0.6 / (m1 + m2, 0.2) ** 0.2
+    return (m1 * m2) ** 0.6 / (m1 + m2) ** 0.2
 
 
 def mchirp_eta2masses(m, eta):
@@ -96,24 +100,27 @@ def network_snr(*snrs):
     return np.sqrt(np.sum([snr**2 for snr in snrs]))
 
 
-def taylor_f2_waveform(m1, m2, dl, iota, fplus, fcross, f):
+def taylor_f2_waveform_face_on(m1, m2, dl, f, pn_order=3.5):
     """
     Compute frequency-domain TaylorF2 waveform approximant up to 3.5 PN
 
-    See Sathyaprakash, B. S. & Schutz, B. F.
+    See Eq. (129) of Sathyaprakash, B. S. & Schutz, B. F.
     Physics, Astrophysics and Cosmology with Gravitational Waves.
     Living Rev. Relativ. 12, 2 (2009).
     """
     eta = symmetric_mass_ratio(m1, m2)
-    m_chirp = mchirp(m1, m2)
-    pn_order = 3.5
+    mtot = msun * (m1 + m2)
+    mchirp_in_kg = msun * mchirp(m1, m2)
     num_coeffs = int(2 * pn_order + 1)
+    dl_in_m = dl * mpc_to_m
 
     # Computing phase \Phi(f) up to 3.5 PN
     # Expansion on x = \pi G M f / c^3
-    a = np.empty(num_coeffs)
-    x = np.pi * G * m_chirp * f / c**3
+    G = const.G.value
+    c = const.c.value
+    x = np.pi * G * mtot * f / c**3
 
+    a = list(np.empty(8))
     a[0] = 1.0
     a[1] = 0.0
     a[2] = 3715 / 756 + 55 / 9 * eta
@@ -132,20 +139,33 @@ def taylor_f2_waveform(m1, m2, dl, iota, fplus, fcross, f):
     a[7] = np.pi * (77096675 / 254016 + 378515 * eta / 1512 - 74045 * eta**2 / 756)
 
     phase = 0.0
-    expansion_param = np.array([np.power(x, (i - 5) / 3) for i in range(num_coeffs)])
-    phase += np.dot(expansion_param, a)
-    phase *= 3 / 128 / eta
-    phase += np.pi / 4
+    for i in range(num_coeffs):
+        phase += a[i] * np.power(x, i / 3)
+    phase *= 3 / 128 / eta / np.power(x, 5 / 3)
+    phase -= np.pi / 4
 
     # Amplitude
-    amp = m_chirp ** (5 / 6) * np.sqrt(5 * eta / 24) / dl / np.pi ** (2 / 3)
-    amp *= f ** (-7 / 6)
+    amp = (G * mchirp_in_kg) ** (5 / 6) * np.sqrt(5 / 24 / c**3) / dl_in_m / np.pi ** (2 / 3)
 
+    # Cutoff
+    risco = 6 * G * mtot / c**2
+    fisco = np.sqrt(G * mtot / risco**3) / np.pi
+    cutoff = np.ones_like(f) * (f < 4 * fisco)
+
+    return cutoff * amp * f ** (-7 / 6) * np.exp(phase * 1.0j), fisco
+
+
+def taylor_f2_orientation(iota, fplus, fcross):
     # Orientation for [plus, cross] polarizations
-    orientation = np.array([0.5 * (1.0 + np.cos(iota) ** 2), np.cos(iota)])
+    hplus, hcross = 0.5 * (1.0 + np.cos(iota) ** 2), np.cos(iota)
     # Amplitude gets factor (F+ h+, Fx hx)^2
-    pattern_function = np.array([fplus, fcross])
-    orientation_factor = pattern_function * orientation
-    amp *= np.sqrt(np.dot(orientation_factor, orientation_factor))
+    # pattern_function = np.array([fplus, fcross])
+    # orientation_factor = pattern_function * orientation
+    # return np.sqrt(np.dot(orientation_factor, orientation_factor))
+    return hplus * fplus + hcross * fcross
 
-    return amp * np.exp(phase * 1.0j)
+
+def taylor_f2_waveform(m1, m2, dl, f, iota, fplus, fcross):
+    waveform_face_on, fisco = taylor_f2_waveform_face_on(m1, m2, dl, f)
+    orientation = taylor_f2_orientation(iota, fplus, fcross)
+    return orientation * waveform_face_on, fisco
