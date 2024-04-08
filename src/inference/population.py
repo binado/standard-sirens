18435f513c9fab41cddd4961b21d434344404863
Bernardo Porto Veronese <bernardo.veronese@arcca.io>
2024-04-08 20:36:11 -0300

    Add BBH mass priors

--- src/inference/population.py
+++ src/inference/population.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from src.utils.math import normalize as _normalize
+from src.utils import math
 
 
 def merger_rate_per_comoving_volume(z, alpha, beta, zp):
@@ -15,6 +15,12 @@ def low_redshift_merger_rate_per_comoving_volume(z, alpha):
     Return un-normalized power-law model for merger rate per comoving volume
     """
     return np.power(1 + z, alpha)
+
+
+def truncated_power_law(x, alpha, xmin, xmax):
+    exp = 1.0 - alpha
+    norm = (xmax**exp - xmin**exp) / exp
+    return np.power(x, -alpha) / norm
 
 
 class UniformComovingRedshiftPrior:
@@ -48,7 +54,7 @@ class MadauDickinsonRedshiftPrior:
         p_cbc = self.dvc_dz_over_1pz * sfr
         # p_cbc is integrated over z in the likelihood numerator and denominator
         # Its overall normalization factor is cancelled out
-        return _normalize(p_cbc, self.z) if normalize else p_cbc
+        return math.normalize(p_cbc, self.z) if normalize else p_cbc
 
     def eval(self, z, alpha, beta, zp, normalize=False):
         """
@@ -64,4 +70,23 @@ class MadauDickinsonRedshiftPrior:
         p_cbc = dvc_dz_over_1pz * sfr
         # p_cbc is integrated over z in the likelihood numerator and denominator
         # Its overall normalization factor is cancelled out
-        return _normalize(p_cbc, z) if normalize else p_cbc
+        return math.normalize(p_cbc, z) if normalize else p_cbc
+
+
+class TruncatedPowerLawPrimaryMassPrior:
+    def __call__(self, m, alpha, mmin, mmax):
+        return truncated_power_law(m, alpha, mmin, mmax)
+
+
+class PowerLawPlusPeakPrimaryMassPrior:
+    def __call__(self, m, alpha, mmin, mmax, f, mu, sigma):
+        power_law = truncated_power_law(m, alpha, mmin, mmax)
+        peak = math.gaussian(m, mu, sigma)
+        return (1 - f) * power_law + f * peak
+
+
+class SecondaryMassPrior(TruncatedPowerLawPrimaryMassPrior):
+    """
+    Class representing the prior for the secondary mass of a black hole binary.
+    It is modeled after a truncated power law, p(m|alpha, mmin, m1)
+    """
