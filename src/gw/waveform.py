e12fbc8a9fc73f0d5eddcadc8fb962b7e88d11f5
Bernardo Porto Veronese <bernardo.veronese@arcca.io>
2024-04-08 20:35:12 -0300

    Implement GW waveform in classes

--- src/gw/waveform.py
+++ src/gw/waveform.py
@@ -1,79 +1,105 @@
+from abc import ABC, abstractmethod
+
 import numpy as np
 from astropy import constants as const
 from astropy import units as u
 
 from .utils import symmetric_mass_ratio, mchirp
+from .event import GWEvent
 
+TAYLOR_F2_IMPLEMENTED_PN_ORDER = 3.5
 mpc_to_m = 1e6 * u.pc.to(u.m)
 msun = const.M_sun.value
+G = const.G.value
+c = const.c.value
 
 
-def taylor_f2_waveform_face_on(m1, m2, dl, f, pn_order=3.5):
-    """
-    Compute frequency-domain TaylorF2 waveform approximant up to 3.5 PN
+class FrequencyDomainWaveform(ABC):
+    def __init__(self, event: GWEvent) -> None:
+        super().__init__()
+        self._event = event
+        self.f_isco = None
 
-    See Eq. (129) of Sathyaprakash, B. S. & Schutz, B. F.
-    Physics, Astrophysics and Cosmology with Gravitational Waves.
-    Living Rev. Relativ. 12, 2 (2009).
-    """
-    eta = symmetric_mass_ratio(m1, m2)
-    mtot = msun * (m1 + m2)
-    mchirp_in_kg = msun * mchirp(m1, m2)
-    num_coeffs = int(2 * pn_order + 1)
-    dl_in_m = dl * mpc_to_m
+    @property
+    def gw(self):
+        return self._event
 
-    # Computing phase \Phi(f) up to 3.5 PN
-    # Expansion on x = \pi G M f / c^3
-    G = const.G.value
-    c = const.c.value
-    x = np.pi * G * mtot * f / c**3
-
-    a = list(np.empty(8))
-    a[0] = 1.0
-    a[1] = 0.0
-    a[2] = 3715 / 756 + 55 / 9 * eta
-    a[3] = -16 * np.pi
-    a[4] = 15293365 / 508032 + 27145 * eta / 504 + 3085 * eta**2 / 72
-    a[5] = np.pi * (38645 / 756 - 65 * eta / 9) * (1.0 + np.log(6**1.5 * x))
-    a[6] = (
-        11583231236531 / 4694215680
-        - (640 / 3) * np.pi**2
-        - 6848 * np.euler_gamma / 21
-        + (-15737765635 / 3048192 + 2255 * np.pi**2 / 12) * eta
-        + 76055 * eta**2 / 1728
-        - 127825 * eta**3 / 1296
-        - 6848 / 63 * np.log(64 * x)
-    )
-    a[7] = np.pi * (77096675 / 254016 + 378515 * eta / 1512 - 74045 * eta**2 / 756)
-
-    phase = 0.0
-    for i in range(num_coeffs):
-        phase += a[i] * np.power(x, i / 3)
-    phase *= 3 / 128 / eta / np.power(x, 5 / 3)
-    phase -= np.pi / 4
-
-    # Amplitude
-    amp = (G * mchirp_in_kg) ** (5 / 6) * np.sqrt(5 / 24 / c**3) / dl_in_m / np.pi ** (2 / 3)
-
-    # Cutoff
-    risco = 6 * G * mtot / c**2
-    fisco = np.sqrt(G * mtot / risco**3) / np.pi
-    cutoff = np.ones_like(f) * (f < 4 * fisco)
-
-    return cutoff * amp * f ** (-7 / 6) * np.exp(phase * 1.0j), fisco
+    @abstractmethod
+    def polarizations(self, f: np.ndarray):
+        pass
 
 
-def taylor_f2_orientation(iota, fplus, fcross):
-    # Orientation for [plus, cross] polarizations
-    hplus, hcross = 0.5 * (1.0 + np.cos(iota) ** 2), np.cos(iota)
-    # Amplitude gets factor (F+ h+, Fx hx)^2
-    # pattern_function = np.array([fplus, fcross])
-    # orientation_factor = pattern_function * orientation
-    # return np.sqrt(np.dot(orientation_factor, orientation_factor))
-    return hplus * fplus + hcross * fcross
+class TaylorF2(FrequencyDomainWaveform):
+    def __init__(self, event: GWEvent, pn_order=TAYLOR_F2_IMPLEMENTED_PN_ORDER) -> None:
+        super().__init__(event)
+        if pn_order > TAYLOR_F2_IMPLEMENTED_PN_ORDER:
+            raise NotImplementedError
 
+        self.pn_order = pn_order
 
-def taylor_f2_waveform(m1, m2, dl, f, iota, fplus, fcross):
-    waveform_face_on, fisco = taylor_f2_waveform_face_on(m1, m2, dl, f)
-    orientation = taylor_f2_orientation(iota, fplus, fcross)
-    return orientation * waveform_face_on, fisco
+    def amplitude(self, f: np.ndarray):
+        gw = self.gw
+        mchirp_in_kg = msun * mchirp(gw.m1d, gw.m2d)
+        dl_in_m = gw.dl * mpc_to_m
+        # Amplitude
+        amp = (G * mchirp_in_kg) ** (5 / 6) * np.sqrt(5 / 24 / c**3) / dl_in_m / np.pi ** (2 / 3)
+        return amp * f ** (-7 / 6)
+
+    def phase_expansion(self, f: np.ndarray):
+        # Computing phase \Phi(f) up to 3.5 PN
+        # Expansion on x = \pi G M f / c^3
+        gw = self.gw
+        eta = symmetric_mass_ratio(gw.m1d, gw.m2d)
+        mtot = msun * (gw.m1d + gw.m2d)
+        num_coeffs = int(2 * self.pn_order + 1)
+        x = np.pi * G * mtot * f / c**3
+
+        a = list(np.empty(8))
+        a[0] = 1.0
+        a[1] = 0.0
+        a[2] = 3715 / 756 + 55 / 9 * eta
+        a[3] = -16 * np.pi
+        a[4] = 15293365 / 508032 + 27145 * eta / 504 + 3085 * eta**2 / 72
+        a[5] = np.pi * (38645 / 756 - 65 * eta / 9) * (1.0 + np.log(6**1.5 * x))
+        a[6] = (
+            11583231236531 / 4694215680
+            - (640 / 3) * np.pi**2
+            - 6848 * np.euler_gamma / 21
+            + (-15737765635 / 3048192 + 2255 * np.pi**2 / 12) * eta
+            + 76055 * eta**2 / 1728
+            - 127825 * eta**3 / 1296
+            - 6848 / 63 * np.log(64 * x)
+        )
+        a[7] = np.pi * (77096675 / 254016 + 378515 * eta / 1512 - 74045 * eta**2 / 756)
+
+        phase = 0.0
+        for i in range(num_coeffs):
+            phase += a[i] * np.power(x, i / 3)
+        phase *= 3 / 128 / eta / np.power(x, 5 / 3)
+        return phase
+
+    def polarizations(self, f: np.ndarray):
+        """
+        Compute frequency-domain TaylorF2 waveform approximant up to 3.5 PN
+
+        See Eq. (129) of Sathyaprakash, B. S. & Schutz, B. F.
+        Physics, Astrophysics and Cosmology with Gravitational Waves.
+        Living Rev. Relativ. 12, 2 (2009).
+        """
+        gw = self.gw
+        plus_phase = self.phase_expansion(f) - np.pi / 4
+        cross_phase = plus_phase + np.pi / 2
+        amp = self.amplitude(f)
+        mtot = msun * (gw.m1d + gw.m2d)
+
+        # Cutoff
+        risco = 6 * G * mtot / c**2
+        self.f_isco = np.sqrt(G * mtot / risco**3) / np.pi
+        cutoff = np.ones_like(f) * (f < 4 * self.f_isco)
+
+        # Polarization due to orientation
+        hplus, hcross = 0.5 * (1.0 + np.cos(gw.iota) ** 2), np.cos(gw.iota)
+        hplus *= cutoff * amp * np.exp(plus_phase * 1.0j)
+        hcross *= cutoff * amp * np.exp(cross_phase * 1.0j)
+
+        return hplus, hcross
